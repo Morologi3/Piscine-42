5c19161a8a3ae382d76de92f07d5301cdc875b63
TechDetective <79792107+Morologi3@users.noreply.github.com>
2024-09-04 17:09:27 +0400

    Add files via upload

---
+++ C03/ft_strlcat.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moalmull <moalmull@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 18:23:48 by moalmull          #+#    #+#             */
+/*   Updated: 2024/09/04 16:42:26 by moalmull         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	k;
+
+	i = ft_strlen(dest);
+	j = ft_strlen(src);
+	if (size <= i)
+	{
+		return (size + j);
+	}
+	k = 0;
+	while (src[k] != '\0' && (i + k) < size - 1)
+	{
+		dest[i + k] = src[k];
+		k++;
+	}
+	dest[i + k] = '\0';
+	return (i + j);
+}
+
+/*
+int	main(void){
+	char a[20] = "Hello, ";
+	char b[] = "World!";
+	printf("%d\n", ft_strlcat(a, b, 20));
+}
+*/
