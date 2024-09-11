91163c3c5a4bfd623387d83aa91b487e4096eade
TechDetective <79792107+Morologi3@users.noreply.github.com>
2024-09-11 16:17:13 +0400

    Add files via upload

--- C02/ft_putstr_non_printable.c
+++ C02/ft_putstr_non_printable.c
@@ -6,13 +6,13 @@
 /*   By: moalmull <moalmull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 16:13:41 by moalmull          #+#    #+#             */
-/*   Updated: 2024/09/04 16:14:00 by moalmull         ###   ########.fr       */
+/*   Updated: 2024/09/09 16:14:06 by moalmull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <unistd.h>
+#include <unistd.h>
 
-void	putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
@@ -35,25 +35,22 @@ void	ft_putstr_non_printable(char *str)
 	{
 		if (ft_str_is_printable(str[i]) == 1)
 		{
-			putchar(str[i]);
+			write(1, &str[i], 1);
 		}
 		else
 		{
-			putchar('\\');
-			putchar("0123456789abcdef"[str[i] / 16]);
-			putchar("0123456789abcdef"[str[i] % 16]);
+			ft_putchar('\\');
+			ft_putchar("0123456789abcdef"[str[i] / 16]);
+			ft_putchar("0123456789abcdef"[str[i] % 16]);
 		}
 		i++;
 	}
 }
 
 /*
-int main (void)
+int	main(void)
 {
-	ft_putstr_non_printable("Coucou\ntu vas bien");
-	putchar('\n');
-	ft_putstr_non_printable("Coucou tu vas bie\t\v\0n");
-	putchar('\n');
-	ft_putstr_non_printable("");
+	ft_putstr_non_printable("Coucou\ntu vas bien ?");
+	ft_putchar('\n');
 }
 */
